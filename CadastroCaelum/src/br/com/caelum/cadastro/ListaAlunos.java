02e4284d56fc3b8f43d1b4e3a36d02995b197c1b
rodrigoandrade <rodrigo.oliveira.and@gmail.com>
2013-11-15 15:15:11 -0200

    Commit Inicial - Aula 1.

---
+++ CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
@@ -0,0 +1,16 @@
+package br.com.caelum.cadastro;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ListaAlunos extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.listagem_alunos);
+		
+	}
+
+}

0acfbcc9c27b4b739d6db27cab41d6694a4ad5cc
rodrigoandrade <rodrigo.oliveira.and@gmail.com>
2013-11-15 21:12:03 -0200

    Aula1.

--- CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
+++ CadastroCaelum/src/br/com/caelum/cadastro/ListaAlunos.java
@@ -2,7 +2,13 @@ package br.com.caelum.cadastro;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListaAlunos extends Activity {
 
@@ -11,6 +17,38 @@ public class ListaAlunos extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.listagem_alunos);
 		
+		String[] nomes = {"Rodrigo", "Alberto", "Maria", "Ana"};
+		int layout = android.R.layout.simple_list_item_1;
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layout, nomes);
+		
+		ListView lista = (ListView) findViewById(R.id.lista);
+		lista.setAdapter(adapter);
+		
+		lista.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View view, int posicao,
+					long id) {
+
+				Toast.makeText(ListaAlunos.this, "Clique na posição "
+						+ posicao, Toast.LENGTH_SHORT).show();
+			}
+			
+		});
+		
+		lista.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View view,
+					int posicao, long id) {
+
+				Toast.makeText(ListaAlunos.this, "Clique longo em " 
+						+ adapter.getItemAtPosition(posicao), Toast.LENGTH_LONG).show();
+				return true;
+			}
+			
+		});
 	}
 
 }
